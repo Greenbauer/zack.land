e5ea9d203b731d8d8de1af1e85a2e699b2f14d1e
Zachary <zacharygreenbauer@gmail.com>
2023-02-23 18:03:36 -0600

    update text

--- components/contents/webApps.tsx
+++ components/contents/webApps.tsx
@@ -2,23 +2,37 @@ import { Content } from '@/types';
 
 const Contents: Content[] = [
   {
+    name: 'This web site',
+    desc: (
+      <p>
+        The web site you are currently viewing, is for me to showcase my skills,
+        experiment, and to master modern web design. I plan on adding many more
+        features to it and will build more useful applications to showcase.
+        Enjoy!
+      </p>
+    ),
+    tech: 'AWS, Serverless, Next.js, Node.js, React, Sass, three.js',
+    repoUrl: 'https://github.com/Greenbauer/zack.land',
+  },
+  {
     name: 'Banner Eyewear',
     desc: (
       <p>
-        Banner Eyewear was developed so people could create and purchase 3D
-        printed sunglasses. The create page has an advanced user interface,
-        where text is warped and extruded, rendering a realistic 3D
-        representation of the real product. Just type text and see that text
-        warped to fit the temples. You can also change the font, ear tip icons,
-        and colors.&nbsp;
+        I developed Banner Eyewear so people could create and purchase 3D
+        printed sunglasses. The create page has a user interface rendering a
+        realistic 3D representation of the real product. Just type some text and
+        see that text fit the sides of the sunglasses in 3D. You can also change
+        the font, ear tip icons, and colors.&nbsp;
         <a href="http://www.shapeways.com" target="_blank" rel="noreferrer">
           Shapeways
         </a>
-        , one of the world’s largest 3D printing companies, has Banner Eyewear
-        featured on their website.
+        , one of the world’s largest 3D printing companies, had Banner Eyewear
+        featured on their website. Today, the WordPress e-commerce site is no
+        longer active. You can still experience the create page however, which
+        lives as a lambda function on AWS.
       </p>
     ),
-    tech: 'jQuery, MySQL, PHP, three.js, webGL, WordPress, WooCommerce',
+    tech: 'jQuery, PHP, three.js, WordPress, Serverless, AWS',
     siteUrl: 'https://banner-eyewear.zack.land',
     src: [
       {
@@ -45,7 +59,7 @@ const Contents: Content[] = [
         <a href="http://www.perfsealers.com" target="_blank" rel="noreferrer">
           Perf Sealers
         </a>
-        &nbsp; is an oil and gas company that sells dissolvable ball sealers. I
+        &nbsp;is an oil and gas company that sells dissolvable ball sealers. I
         help with many projects, including building and maintaining their
         website.
       </p>
@@ -59,19 +73,6 @@ const Contents: Content[] = [
         type: 'img',
       },
     ],
-  },
-  {
-    name: 'This web site',
-    desc: (
-      <p>
-        The web site you are currently viewing, is for me to showcase my skills,
-        experiment, and to master modern web design. I plan on adding many more
-        features to it and will build more useful applications to showcase.
-        Enjoy!
-      </p>
-    ),
-    tech: 'AWS, Serverless, Next.js, Node.js, React, Sass, three.js',
-    repoUrl: 'https://github.com/Greenbauer/zack.land',
   },
 ];
 
